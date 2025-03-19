ceba05670e2a4d0e8a345624ebf804ef69219e12
KartikAmbupe <kartikambupe@gmail.com>
2025-03-19 11:39:38 +0530

    NB trained

--- models/predict.py
+++ models/predict.py
@@ -8,6 +8,8 @@ from nltk.stem import WordNetLemmatizer
 # Load trained model & vectorizer
 model = joblib.load("models/trained/nb_news_classifier.pkl")
 vectorizer = joblib.load("models/trained/nb_tfidf_vectorizer.pkl")
+model = joblib.load("nb_news_classifier.pkl")
+vectorizer = joblib.load("nb_tfidf_vectorizer.pkl")
 
 # Preprocessing function
 lemmatizer = WordNetLemmatizer()
@@ -30,7 +32,11 @@ def predict_news(news_text):
 
 # Test
 sample_text = """
+
 The government has introduced a new healthcare policy aimed at providing affordable medical services to all citizens. The policy includes subsidized health insurance and expanded access to rural areas
+=======
+According to a report circulating on WhatsApp, the United Nations has officially recognized India as the richest country in the world in 2024. The message claims that India’s economy has surpassed that of the USA and China in terms of GDP.
+>>>>>>> e8c4308 (NB trained)
 """
 
 """Real
